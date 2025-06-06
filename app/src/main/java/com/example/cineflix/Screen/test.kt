e69d8189f56a1b100d004573d5a45edd734b9d94
shri2104 <shrikants0421@gmail.com>
2025-06-06 15:49:34 +0530

    WhosWatchingScreen

---
+++ app/src/main/java/com/example/cineflix/Screen/test.kt
@@ -0,0 +1,129 @@
+package com.example.cineflix.Screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.example.cineflix.R
+
+
+@Composable
+fun WhosWatchingScreen() {
+    val profiles = listOf(
+        Profile("Shrikant S...", painterResource(R.drawable.prof)),
+        Profile("Children", painterResource(R.drawable.child))
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .padding(top = 16.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Spacer(modifier = Modifier.width(1.dp))
+                Text(
+                    text = "Who’s Watching?",
+                    style = MaterialTheme.typography.titleLarge.copy(
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                Text(
+                    text = "Edit",
+                    color = Color.White,
+                    modifier = Modifier.clickable {  }
+                )
+            }
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                horizontalArrangement = Arrangement.Center,
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp)
+            ) {
+                items(profiles) { profile ->
+                    ProfileItem(profile)
+                }
+
+                item {
+                    AddProfileButton()
+                }
+            }
+        }
+    }
+}
+
+data class Profile(val name: String, val image: Painter)
+
+@Composable
+fun ProfileItem(profile: Profile) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = profile.image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(100.dp)
+                .clip(RoundedCornerShape(16.dp))
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = profile.name,
+            color = Color.White,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
+@Composable
+fun AddProfileButton() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(16.dp)),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(Icons.Default.Add, contentDescription = "Add", tint = Color.White)
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "Add Profile", color = Color.White)
+    }
+}
